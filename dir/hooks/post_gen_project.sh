ad2bb4030069e1bae16bd6b0610e2188b9e5c9f1
Tim Hopper <tim.hopper@TimHopperMBP.attlocal.net>
2021-04-21 15:17:06 -0400

    Don't use global config 😅

--- dir/hooks/post_gen_project.sh
+++ dir/hooks/post_gen_project.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 git init -q
 
-git config --global user.email "you@example.com"
-git config --global user.name "Your Name"
+git config user.email "you@example.com"
+git config user.name "Your Name"
 git add --all
 git commit --message "initial"
 
