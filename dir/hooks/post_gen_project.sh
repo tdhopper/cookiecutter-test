7e3c8f26cd90bc5393b792a205cab86ffa8fd968
Your Name <you@example.com>
2021-04-21 16:34:43 -0400

    use urandom

--- dir/hooks/post_gen_project.sh
+++ dir/hooks/post_gen_project.sh
@@ -1,8 +1,2 @@
 #!/usr/bin/env sh
-git init -q
-
-git config user.email "you@example.com"
-git config user.name "Your Name"
-git add --all
-git commit --message "initial"
-
+cat /dev/urandom > random_data 
